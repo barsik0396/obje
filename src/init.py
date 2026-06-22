adf4214c3f505406318a7bd084ee0761b021fdf2
barsik0396 <Barsik0396@outlook.com>
2026-06-22 16:43:53 +0300

    add config loader and continue writing updater part

--- src/init.py
+++ src/init.py
@@ -3,6 +3,7 @@ import os
 import time
 import threading
 import requests
+import json
 
 # update checker
 def check_for_updates():
@@ -21,6 +22,28 @@ def check_for_updates():
 
 # init
 def init():
+    # -- load config --
+    if not os.path.isdir(os.path.expanduser("~/.obje/")):
+        os.makedirs(os.path.expanduser("~/.obje/"), exist_ok=True)
+    if os.path.isfile(os.path.expanduser("~/.obje/config.json")):
+        try:
+            with open(os.path.expanduser("~/.obje/config.json"), "rb") as f:
+                config_text = f.read()
+            config = json.loads(config_text)
+        except json.decoder.JSONDecodeError:
+            print("Failed to parse config")
+            sys.exit(1)
+    else:
+        try:
+            _ = requests.get("https://raw.githubusercontent.com/barsik0396/obje/refs/heads/main/config.json.example")
+            with open(os.path.expanduser("~/.obje/config.json"), "wb") as f:
+                f.write(_.content)
+        except Exception as e:
+            print("Failed to download config:")
+            print(f" {e}")
+            sys.exit(1)
+        init()
+
     # -- help --
     if len(sys.argv) < 2:
         print("Obje - \x1b[37;1mModern and fast build system\x1b[0m")
@@ -31,7 +54,7 @@ def init():
         print("  \x1b[37;1mCOMMANDS\x1b[0m")
         print("    update                   Update Obje")
     else:
-        # -- update --
+        # -- updater --
         if sys.argv[1] == "update":
             global done
             done = False
@@ -55,9 +78,6 @@ def init():
                 if upd_request.status_code == 404:
                     print("\r\x1b[1;31m✗\x1b[0;91m Server returned 404")
                     sys.exit(1)
-                
-                # TODO: finish this part
-                print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
             # -- if error --
             except Exception:
                 try:
@@ -66,3 +86,19 @@ def init():
                     # fallback
                     print("\r\x1b[0m                                       \r", end="")
                 sys.exit(1)
+
+
+            # --- update data ---
+            data = json.loads(upd_request.content)
+            latest_stable = data["latest"]["stable"]
+            latest_pre = data["latest"]["pre"]
+            latest_nightly = data["latest"]["nightly"]
+
+            dl_stable = data["download"]["stable"]
+            dl_pre = data["download"]["stable"]
+            dl_nightly = data["download"]["stable"]
+            
+            # if latest_(channel) == (installed version) -> ok
+            # TODO: finish this part
+            if latest_stable == "v0.1.0":
+                print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
