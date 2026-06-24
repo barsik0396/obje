87acdb2017eda919020ff2481431e2cf7ecde3b8
barsik0396 <Barsik0396@outlook.com>
2026-06-24 13:05:53 +0300

    add env support to debugger

--- src/dbg.py
+++ src/dbg.py
@@ -1,5 +1,13 @@
 from . import config
+import os, sys
 
 def debug(text):
-    if config.data["debugger"]["enabled"]:
+    if os.environ.get("OBJE_DEBUGGER_ENABLED", "false") == "false":
+        enabled = False
+    elif os.environ.get("OBJE_DEBUGGER_ENABLED", "false") == "true":
+        enabled = True
+    else:
+        print("error: ")
+        sys.exit(1)
+    if config.data["debugger"]["enabled"] or enabled:
         print(f"{config.data["debugger"]["prefix"]}{text}")
