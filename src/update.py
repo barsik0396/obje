51cb85bee5ff54e3067a72ffe4ed173b20b4bd55
barsik0396 <Barsik0396@outlook.com>
2026-06-24 13:06:11 +0300

    update updater...

--- src/update.py
+++ src/update.py
@@ -26,6 +26,7 @@ def check_for_updates():
     dbg("upd: done downloading update.json")
     done = True
 
+
 # -- main update logic --
 def update():
     config.load()
@@ -91,8 +92,64 @@ def update():
     
     
 
-    # if latest_(channel) == (installed version) -> ok
+    # -- update installer --
     # TODO: finish this part
-    dbg("hi")
-    if latest_stable == "v0.1.0":
-        print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
+    if not config.data["updates-channel"] in "nightly pre stable":
+        print("error: ...")
+        sys.exit(1)
+    ch = config.data["updates-channel"]
+    # --- check ---
+    if ch == "nightly":
+
+        if latest_nightly == "v0.0.9":
+            print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
+            sys.exit(1)
+        else:
+            print("\r\x1b[1;32mNew nightly build found!\x1b[0m")
+    elif ch == "pre":
+        if latest_pre == "v0.0.9":
+            print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
+            sys.exit(1)
+        else:
+            print("\r\x1b[1;32mNew pre build found!\x1b[0m")
+    elif ch == "stable":
+        if latest_stable == "v0.0.9":
+            print("\r\x1b[1;32m✔\x1b[0;92m You have latest version of Obje!\x1b[0m")
+            sys.exit(1)
+        else:
+            print("\r\x1b[1;32mNew release found!\x1b[0m")
+    else:
+        print("error: unk channel")
+        sys.exit(1)
+    
+    # --- dl and install ---
+    global done2
+    done2 = False
+    frames = ['⠋','⠙','⠹','⠸','⠼','⠴','⠦','⠧','⠇','⠏']
+    if ch == "nightly":
+        print("Downloading update...")
+        try:
+            dbg("showing spinner")
+            while not done2:
+                for item in frames:
+                    if done2: 
+                        break
+                    dbg(f"\r{config.data["debugger"]["prefix"]}spinner: next frame")
+                    print(f"\r\x1b[96m{item}\x1b[0m", end="", flush=True)
+                    try:
+                        time.sleep(0.1)
+                    except KeyboardInterrupt:
+                        print("\r   \r\x1b[0m", end="")
+                        sys.exit(0)
+            print("\r \r", end="")
+            if upd_request.status_code == 404:
+                print("\r\x1b[1;31m✗\x1b[0;91m Server returned 404")
+                sys.exit(1)
+        except json.decoder.JSONDecodeError:
+            print("ok")
+        # except Exception:
+        #     try:
+        #         print(f"\r\x1b[0m{" " * os.get_terminal_size().columns}\r", end="")
+        #     except Exception:
+        #         print("\r\x1b[0m   \r", end="")
+        #         sys.exit(1)
